3b5078a21c073ab4fedf1aaa34095537996365ee
arthurRosaDaSilva <arthurrosasilva324@gmail.com>
2025-05-23 16:33:18 -0300

    Update codigoCadastro.py

--- codigoCadastro.py
+++ codigoCadastro.py
@@ -1,10 +1,10 @@
 cadastro_login = input("Crie seu Login: ")
 cadastro_senha = input("Crie uma senha: ")
-usuario = input("Digite seu login")
-senha = input("Digite sua senha")
-
+usuario = input("Digite seu login: ")
+senha = input("Digite sua senha: ")
+print("---------------------------------------------------------------------")
 while cadastro_login != usuario or cadastro_senha != senha:
     print("Usuario invalido, tente novamente")
-    usuario = input("Digite seu login")
-    senha = input("Digite sua senha")
-print("usuario valido")
+    usuario = input("Digite seu login: ")
+    senha = input("Digite sua senha: ")
+print("usuario valido !!!")
